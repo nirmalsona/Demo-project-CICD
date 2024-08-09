c84f06166f3c44c098608c4fd741d0494654fcf1
Nirmal Sona <126976548+nirmalsona@users.noreply.github.com>
2024-08-10 02:08:57 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ ls
 pwd
 whoami
 sudo apt install docker.io -y
-sudo systemctl start docker
-sudo systemctl enable docker
+systemctl start docker
+systemctl enable docker
 sudo docker run -dt --name nginx -p 2000:80 nginx /bin/bash
 sudo docker start nginx
