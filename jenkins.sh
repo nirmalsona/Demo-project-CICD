aa2add758e9792ea745442d0ce17b9a901c79c26
Nirmal Sona <126976548+nirmalsona@users.noreply.github.com>
2024-08-10 01:18:07 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -6,3 +6,5 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker run -dt --name nginx -p 2000:80 nginx /bin/bash
 sudo docker start nginx
+sudo docker exec -it nginx /bin/bash
+nginx
